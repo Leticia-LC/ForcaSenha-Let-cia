4c7e4a74b78551014090e01c6cc220345eff2728
Letícia de Lima da Cunha <leticia.lc2005@aluno.ifsc.edu.br>
2023-10-25 21:44:00 -0300

    ajustando mais um pouco

--- main.js
+++ main.js
@@ -1,90 +1,76 @@
-const inputSenha = document.querySelector("idSenha")
-        let forcaSenha = 0
-        let saida = document.querySelector(".barraProtecao")
-        let arrayNumeros = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-        let arrayMin = ["a", "b", "c", , "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-        let arrayMax = ["A", "B", "C", "Ç", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-        let arrayCaracteres = ["\"", "'", "#", "$", "%", "&", "*", "(", ")", "_", "+", "-", "=", "{", "[", "]", "}", "^", "/", "?", "~", ";", ":", ".", ">", ",", "<", "\\", "|", "@"]
+let senha = document.getElementById("idSenha")
+let barra = document.querySelector(".status")
+let faixa = document.getElementById("idFaixa")
+let saida = document.getElementById("idOut")
+let mostrarSenha = document.getElementById("idButton")
+let arrayNumeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+let arrayMinusculas = ["a", "b", "c", "ç", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+let arrayMaiusculas = ["A", "B", "C", "Ç", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+let arrayCaracteres = ["\"", "'", "#", "$", "%", "&", "*", "(", ")", "_", "+", "-", "=", "{", "[", "]", "}", "^", "/", "?", "~", ";", ":", ".", ">", ",", "<", "\\", "|", "@"];
 
-        function mostrarSenha(){
-            let botao = document.querySelector(".idButton")
+mostrarSenha.addEventListener("click", function(){
+    if(senha.type === "password"){
+        senha.type = "text";
+        document.getElementById("idButton").innerText = "HIDE"
+    } else {
+        senha.type = "password";
+        document.getElementById("idButton").innerText = "SHOW"
+    }
 
-            if(botao.textContent == "SHOW"){
-                botao.textContent = "HIDE"
+senha.addEventListener("input", function(){
+    let count = 0;
+    let senhaInput = element.target.value;
+    let progresso = element.target.value.length;
+    let barraProgresso = document.querySelector(".barraProgresso")
 
-            } else {
-                botao.textContent = "SHOW"
-            }
+    if(progresso >= 1 && progresso <= 6){
+        count += 1;
+
+    } else if(progresso > 6 && progresso < 10){
+        count += 2;
+
+    } else if(progresso >= 10 && progresso < 12){
+        count += 3;
+
+    } else if(progresso >= 12 && progresso < 14){
+        count += 4;
+
+    } else if(progresso >= 14){
+        count += 5;
+
+    }
+
+    for(let i = 0; i < senha.length; i++){
+        if((arrayNumeros.toString()).includes(senha[i])){
+            count += 1;
+            break;
         }
+    }
 
-        inputSenha.focus();
-        
-        inputSenha.addEventListener("input", (element) =>{
-            let senha = document.querySelector("idSenha").value
 
-            if(senha.lenght > 1 && senha.length < 6){
-                forcaSenha += 1;
+    for(let i = 0; i < senha.length; i++){
+        if(arrayMinusculas.includes(senha[i])){
+            count += 1;
+            break;
+        }
+    }
+           
+    for(let i = 0; i < senha.length; i++){
+        if(arrayMaiusculas.includes(senha[i])){
+            count += 1;
+            break;
+        }
+    }
 
-            } else if(senha.length > 6 && senha.length < 10){
-                forcaSenha += 2;
 
-            } else if(senha.length >= 10 && senha.length < 12){
-                forcaSenha += 3;
+    for(let i = 0; i < senha.length; i++){
+        if(arrayCaracteres.includes(senha[i])){
+            count += 2;
+            break;
+        }
+    }
 
-            } else if(senha.length >= 12 && senha.length < 14){
-                forcaSenha += 4;
 
-            } else if(senha.length >= 14){
-                forcaSenha += 5;
-            }
+})
 
-            for(let i = 0; i < senha.length; i++){
-                if((arrayNumeros.toString()).includes(senha[i])){
-                    forcaSenha += 1;
-                    break;
-                }
-            }
-
-            for(let i = 0; i < senha.length; i++){
-                if(arrayMin.includes(senha[i])){
-                    forcaSenha += 1;
-                    break;
-                }
-            }
-            
-            for(let i = 0; i < senha.length; i++){
-                if(arrayMax.includes(senha[i])){
-                    forcaSenha += 1;
-                    break;
-                }
-            }
-
-            for(let i = 0; i < senha.length; i++){
-                if(arrayCaracteres.includes(senha[i])){
-                    forcaSenha += 2;
-                    break;
-                }
-            }
-
-            forcaSenha = Math.min(forcaSenha, 10);
-
-            let barraProtecao = document.querySelector(".protecaoSenha");
-            let barraProtecaoWidth = pontos * 10;
-
-            barraProtecao.computedStyleMap.width = barraProtecaoWidth + '%';
-
-            if (pontos >= 9) { 
-                document.documentElement.style.setProperty("--color", "#0f0");
-                barraProgresso.style.backgroundColor = "#0f0";
-            }
-            if (pontos > 5 && pontos < 9) { 
-                document.documentElement.style.setProperty("--color", "#fded00");
-                barraProgresso.style.backgroundColor = "#fded00";
-            }
-            if (pontos <= 5) { 
-                document.documentElement.style.setProperty("--color", "#bd1b4e");
-                barraProgresso.style.backgroundColor = "#bd1b4e";
-            }
-
- 
-        });
+});
